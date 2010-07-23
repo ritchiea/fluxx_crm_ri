2bc0fa9d874a0936a1bbefb53fbb4591940484be
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-07-23 10:57:31 -0700

    customize the org form and show

--- db/schema.rb
+++ db/schema.rb
@@ -329,7 +329,7 @@ ActiveRecord::Schema.define(:version => 20100723164103) do
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
-  add_index "users", ["email"], :name => "index_users_on_email"
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["login"], :name => "index_users_on_login", :unique => true
   add_index "users", ["personal_geo_country_id"], :name => "users_personal_country_id"
   add_index "users", ["personal_geo_state_id"], :name => "users_personal_geo_state_id"
