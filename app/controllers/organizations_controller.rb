55436c4e7e7fe810adee8ef54a3847ab5be532d1
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-08-30 21:38:19 -0700

    switch to rails 3.0.0.  Put related data for org controller directly in org controller

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -5,4 +5,17 @@ class OrganizationsController < ApplicationController
     insta.filter_title = "Organizations Filter"
     insta.filter_template = 'organizations/organization_filter'
   end
+  
+  base.insta_related Organization do |insta|
+    insta.add_related do |related|
+      related.display_name = 'People'
+      related.related_class = User
+      related.search_id = :organization_id
+      related.extra_condition = {:deleted_at => 0}
+      related.max_results = 20
+      related.order = 'last_name asc, first_name asc'
+      related.display_template = '/users/related_users'
+    end
+  end
+  
 end
